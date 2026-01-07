b091f81ea8fe9502089f97776dcce1d87bd25e39
unknown <vladimirovicp70@gmail.com>
2026-01-07 23:07:17 +0400

    feat: add three opened and closed

---
+++ src/assets/js/components/treeView/treeView.js
@@ -0,0 +1,55 @@
+
+
+export function treeView(){
+    console.log('tree-view');
+
+    const tree = document.querySelector('.tree-view');
+    if(!tree){
+        return;
+    }
+
+    const treeItems = tree.querySelectorAll('.tree-item');
+
+    treeItems.forEach((item) => {
+        
+        item.addEventListener('click', () => {
+            const title = item.querySelector('.tree-item__title');
+            if(title){
+                console.log(title.textContent);
+            }
+
+            const parent = item.parentElement;
+            if (parent) {
+
+                if(parent.classList.contains('folder')){
+                    const isOpened = parent.classList.contains('opened');
+                    parent.classList.toggle('opened', !isOpened);
+                    parent.classList.toggle('closed', isOpened);
+                } else if(parent.classList.contains('file')){
+
+                    console.log(item);
+
+                } else{
+                    console.log('Error treeViev');
+                }
+
+
+            } 
+
+            
+
+            //const folder = item.closest('.view.folder');
+
+            //console.log(folder);
+
+            // if(folder){
+            //     const isOpened = folder.classList.contains('opened');
+            //     folder.classList.toggle('opened', !isOpened);
+            //     folder.classList.toggle('closed', isOpened);
+            // } else{
+            //     console.log('file');
+            // }
+        });
+    });
+
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,7 @@
 import { resizable } from './helpers/resizable.js';
-
+import { treeView} from './components/treeView/treeView.js'
 
 
 resizable('.divider', '.tree-view', '.gp__main');
+
+treeView();
