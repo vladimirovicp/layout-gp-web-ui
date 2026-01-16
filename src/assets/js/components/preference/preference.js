7c5b8c6a258e9b6bb1552030216e2f559132566e
John Doe <johndoe@example.com>
2026-01-16 16:56:57 +0400

    feat: add table

--- src/assets/js/components/preference/preference.js
+++ src/assets/js/components/preference/preference.js
@@ -2,8 +2,6 @@ import { resizable } from '../../helpers/resizable.js';
 
 export function preference(){
 
-    console.log('preference');
-
     const workspace = document.querySelector('.workspace');
     if(workspace){
         const preference = workspace.querySelector('.gp__preference');
@@ -13,9 +11,28 @@ export function preference(){
             if(divider){
                 resizable('.preference__divider', '.preference__info', '.gp__preference');
             }
-        }
-
-        
+        } 
     }
+
+    //Реакция клика правой кнопки на таблицу, делаем её активной
+
+    if(workspace){
+        const table = workspace.querySelector('.preference__table');
+
+        table.addEventListener('click', function(event) {
+        // Игнорируем клики не по строкам tbody
+        const row = event.target.closest('tr');
+        if (!row || !table.contains(row) || row.querySelector('th')) return;
+
+        // Удаляем класс 'active' у всех строк
+        table.querySelectorAll('tr.active').forEach(tr => tr.classList.remove('active'));
+
+        // Добавляем класс 'active' к текущей строке
+        row.classList.add('active');
+    });
+
+    }
+
+
 
 }
