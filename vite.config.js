a14d3189c7c45d3084918d18e37e9efe9f82af60
unknown <vladimirovicp70@gmail.com>
2026-03-03 19:44:09 +0400

    feat: refactor del multy pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vite';
-import {resolve} from 'path';
-
-const FRONT_PATH = 'src';
 
 export default defineConfig({
     root: "src",
@@ -9,14 +6,7 @@ export default defineConfig({
     build: {
         minify: true,
         cssMinify: true,
-       minifyCSS: 'lightningcss',
-       rollupOptions: {
-            input: {
-                index: resolve(__dirname, 'src/index.html'),
-                list: resolve(__dirname, 'src/pages/list/index.html'),
-                test: resolve(__dirname, `${FRONT_PATH}/pages/test/index.html`),
-            }
-        },
+        minifyCSS: 'lightningcss',
         outDir: '../dist',
-   },
+    },
 });
